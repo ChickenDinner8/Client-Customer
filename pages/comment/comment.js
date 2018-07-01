7afce3ac9e61e435d7e814d6d122f42d6168389a
Pomeloqy <1056805770@qq.com>
2018-07-02 00:35:19 +0800

    add 图片圆角化

--- pages/comment/comment.js
+++ pages/comment/comment.js
@@ -3,13 +3,8 @@ Page({
   /**
    * 页面的初始数据
    */
-  onLoad: function () {
-    this.setData({
-      //icon20: base64.icon20,
-      //icon60: base64.icon60
-    });
-  },
   data: {
+    comment_count: '0',
     comment: [{
       img: '../../images/comment_img1.jpg',
       userImg: '../../images/userimg1.png',
@@ -17,7 +12,8 @@ Page({
       date: '2018-05-28',
       desc: '好吃的菜，可爱的人，有趣的心灵，还会再来的。',
       likeCount: '31',
-      unlikeCount: '5'
+      unlikeCount: '5',
+      index: '0'
     }, {
       img: '../../images/comment_img3.jpg',
       userImg: '../../images/userimg3.png',
@@ -25,14 +21,23 @@ Page({
       date: '2018-04-25',
       desc: '恭喜爱我们总是帮我们debug的zj爸爸又成熟一岁，今天吃的很开心，喜欢这家餐厅，喜欢704在一起的每一天',
       likeCount: '101',
-      unlikeCount: '0'
+      unlikeCount: '0',
+      index: '1'
     }]
   }, 
+
+  onLoad: function () {
+    this.setData({
+      comment_count: this.data.comment.length.toString()
+    });
+    console.log('comment_count', this.data.comment_count)
+  },
+
   toCommit: function() {
     wx.showToast({
       title: '功能正在开发中',
       icon: 'loading',
-      duration: 3000
+      duration: 1000
     })
   }
 });
